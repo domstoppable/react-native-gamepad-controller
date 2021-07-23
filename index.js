ca42cbc944bd0a643bd5dd0de667525dbaf89377
yongtheskill <yongee2003@gmail.com>
2021-07-23 22:25:00 +0800

    Update webview import
    Import webview from react-native-webview instead of react-native

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, WebView } from 'react-native';
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 var js = `
 	var haveEvents = 'GamepadEvent' in window;
